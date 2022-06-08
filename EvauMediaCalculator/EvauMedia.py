c60863d6405f6aed0c9b32ba5ad45cef8541d2c8
opensuse <opensuse@localhost.localdomain>
2022-06-08 19:21:54 +0200

    Corregido la funcion de Calculo y el resultado

--- EvauMediaCalculator/EvauMedia.py
+++ EvauMediaCalculator/EvauMedia.py
@@ -15,22 +15,6 @@ def ciencias():
     frame = tk.Frame(nuevaventanauno, bg="white")
     frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
 
-    def Calculo():
-        canvas = tk.Canvas(nuevaventanauno, height=400, width=200, bg="#A9EFFE")
-        canvas.pack()
-
-        frame = tk.Frame(nuevaventanauno, bg="white")
-        frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
-
-        StringMedia = "Tu nota media actual es" + str(Media)
-        StringNotaEvau = "Tu nota actual en la Evau es" + str(NotaEvau)
-
-        MediaFinal = tk.Label(frame,text=StringMedia)
-        MediaFinal.pack()
-
-        EvauFinal = tk.Label(frame, text=StringNotaEvau)
-        EvauFinal.pack()
-
     Texto2 = tk.Label(frame,text="Inserta tus notas:")
     Texto2.pack()
 
@@ -88,22 +72,40 @@ def ciencias():
     Optativa2 = tk.Entry(frame)
     Optativa2.pack()
 
-    NotaMates = int(matematicas)
-    NotaLengua = int(LenguaCiencias)
-    NotaFilo = int(FilosofiaCiencias)
-    NotaIngles = int(InglesCiencias)
-    NotaEduca = int(EducaCiencias)
-    NotaFYQ = int(Fisica)
-    NotaDibujoBig = int(DibujoBio)
-    NotaOptativa1 = int(Optativa1)
-    NotaOptativa2 = int(Optativa2)
+    def Calculo():
+        canvas = tk.Canvas(nuevaventanauno, height=400, width=200, bg="#A9EFFE")
+        canvas.pack()
 
-    Suma = (NotaDibujoBig + NotaEduca + NotaFilo + NotaFYQ + NotaIngles +NotaLengua + NotaMates + NotaOptativa1 +NotaOptativa2)
-    Media = int(Suma)/9
-    NotaEvau = int(Media)*0,3
+        frame = tk.Frame(nuevaventanauno, bg="white")
+        frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
 
-    Calcular = tk.Button(frame, text= "Calcular",command=Calculo)
+        NotaMates = int(matematicas.get())
+        NotaLengua = int(LenguaCiencias.get())
+        NotaFilo = int(FilosofiaCiencias.get())
+        NotaIngles = int(InglesCiencias.get())
+        NotaEduca = int(EducaCiencias.get())
+        NotaFYQ = int(Fisica.get())
+        NotaDibujoBig = int(DibujoBio.get())
+        NotaOptativa1 = int(Optativa1.get())
+        NotaOptativa2 = int(Optativa2.get())
+
+        Suma = (NotaDibujoBig + NotaEduca + NotaFilo + NotaFYQ + NotaIngles + NotaLengua + NotaMates + NotaOptativa1 + NotaOptativa2)
+        Media = Suma/9
+        NotaEvau = Media*0.3
+
+        StringMedia = "Tu nota media actual es " + str(Media)
+        StringNotaEvau = "Tu nota actual en la Evau es" + str(NotaEvau)
+
+        MediaFinal = tk.Label(frame,text=StringMedia)
+        MediaFinal.pack()
+
+        EvauFinal = tk.Label(frame, text=StringNotaEvau)
+        EvauFinal.pack()
+
+    Calcular = tk.Button(frame, text="Calcular",command=Calculo)
     Calcular.pack()
+
+
 
 def NotaActualFuncion():
     nuevaventana = tk.Toplevel(root)
